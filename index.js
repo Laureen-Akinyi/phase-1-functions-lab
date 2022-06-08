c9aed225d021b359995987ed6d68656498af7111
laureenakinyi <laureen.akinyi@student.moringaschool.com>
2022-06-08 09:26:51 +0300

    commit tree 9142234ae356ce6e87de615ee01063849a9b5f88

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(blocks){
+    if (blocks>42){
+        return (blocks-42)
+    }else {
+        return (42-blocks) 
+    }
+
+}
+function distanceFromHqInFeet(blocks) {
+    return distanceFromHqInFeet(blocks)*264;
+ }
+ function distanceTravelledInFeet(start, destination){
+     if (start<destination)
+     return (destination-start)*264
+     else{
+        return (start-destination)*264
+
+     }
+ }
+
+
+    
